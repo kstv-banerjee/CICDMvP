afa25c92814a78797188fa0059c8cb3496029360
kstv-banerjee <kstv.banerjee@gmail.com>
2019-03-15 15:24:56 +0100

    Update Config.sh

--- Config.sh
+++ Config.sh
@@ -1,4 +1,3 @@
-
 aws s3 cp s3://jenkinsconfigtemplate/jenkins_homeACtemplate . --recursive
 sed -i "s/Url><\/jenkinsUrl/Url>http:\/\/$LBDns\/<\/jenkinsUrl/g" config.xml
 sed -i "s/tunnel><\/tunnel/tunnel>$LBDns:50000<\/tunnel/g" config.xml
