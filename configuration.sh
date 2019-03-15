0422888c3e4c8b675cd7bd7106dc395c8497b0d7
kstv-banerjee <kstv.banerjee@gmail.com>
2019-03-15 15:58:37 +0100

    Add files via upload

---
+++ configuration.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+aws s3 cp --recursive s3://jenkinsconfigtemplate/jenkins_homeACtemplate .
+sed -i "s/Url><\/jenkinsUrl/Url>http:\/\/$LBDns\/<\/jenkinsUrl/g" config.xml
+sed -i "s/tunnel><\/tunnel/tunnel>$LBDns:50000<\/tunnel/g" config.xml
+sed -i "s/cluster><\/cluster/cluster>arn:aws:ecs:$Region:$Accountid:cluster\/$Clustername<\/cluster/g" config.xml
+sed -i "s/regionName><\/regionName/regionName>$Region<\/regionName/g" config.xml
