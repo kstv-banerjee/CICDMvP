9b45ac68c691cc828930736a26a1a73f638eb6b9
kstv-banerjee <kstv.banerjee@gmail.com>
2019-03-18 18:17:35 +0100

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -2,6 +2,7 @@
 
 aws s3 cp --recursive s3://jenkinsconfigtemplate/jenkins_homeACtemplate .
 sed -i "s/Url><\/jenkinsUrl/Url>http:\/\/$LBDns\/<\/jenkinsUrl/g" config.xml
+sed -i "s/Url><\/jenkinsUrl/Url>http:\/\/$LBDns\/<\/jenkinsUrl/g" jenkins.model.JenkinsLocationConfiguration.xml
 sed -i "s/tunnel><\/tunnel/tunnel>$LBDns:50000<\/tunnel/g" config.xml
 sed -i "s/cluster><\/cluster/cluster>arn:aws:ecs:$Region:$Accountid:cluster\/$Clustername<\/cluster/g" config.xml
 sed -i "s/regionName><\/regionName/regionName>$Region<\/regionName/g" config.xml
