06eecabf23a2e8b5b1a961dea05362012df7fd09
kstv-banerjee <kstv.banerjee@gmail.com>
2019-03-25 15:53:04 +0100

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -5,4 +5,5 @@ sed -i "s/Url><\/jenkinsUrl/Url>http:\/\/$LBDns\/<\/jenkinsUrl/g" config.xml
 sed -i "s/Url><\/jenkinsUrl/Url>http:\/\/$LBDns\/<\/jenkinsUrl/g" jenkins.model.JenkinsLocationConfiguration.xml
 sed -i "s/tunnel><\/tunnel/tunnel>$LBDns:50000<\/tunnel/g" config.xml
 sed -i "s/cluster><\/cluster/cluster>arn:aws:ecs:$Region:$Accountid:cluster\/$Clustername<\/cluster/g" config.xml
+sed -i "s/taskDefinitionArn><\/taskDefinitionArn/taskDefinitionArn>arn:aws:ecs:$Region:$Accountid:task-definition\/jenkins-slave:1<\/taskDefinitionArn/g" config.xml
 sed -i "s/regionName><\/regionName/regionName>$Region<\/regionName/g" config.xml
